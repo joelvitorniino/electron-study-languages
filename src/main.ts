50be9d9350eac1fbde6de8ad293d7215a7378ddc
joelvitorniino <joelprofessor13@gmail.com>
2023-03-10 16:35:36 -0300

    build: project created

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { app, BrowserWindow } from 'electron';
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: `${__dirname}/preload.js`
+        }
+    });
+    
+    win.loadFile("./index.html");
+};
+
+app.whenReady()
+    .then(() => createWindow());
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+});
